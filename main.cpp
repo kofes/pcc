8ecad69067c885c64c4829a230bc6d90d9e5b7fb
kofes <turnever@gmail.com>
2017-03-25 13:24:32 +1000

    tmp:parser/scanner -> main

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "inc/Scanner.hpp"
+#include "inc/Parser.hpp"
+
+const std::string help =
+  "Usage\n\n"
+  "   <exec_file> [option] <file>\n\n"
+  "Options\n\n"
+  "   -s, --scanner   - Lexer -> print all lexems from <file> to &1\n"
+  "   -p, --parser    - Parser -> print syntax tree of expressions from <file> to &1\n"
+  "   -h, --help      - Print 'help'\n\n"
+  "<file> <- 'test.pas', if not setted.\n";
+
+int main(int argc, char const *argv[]) {
+  if (argc < 2 || !std::strcmp(argv[1], "--help") || !std::strcmp(argv[1], "-h")) {
+    std::cout << help << std::endl;
+    return 0;
+  }
+
+  // SCANNER
+  if (!std::strcmp(argv[1], "--scanner") || !std::strcmp(argv[1], "-s")) {
+    compiler::Scanner scanner;
+    compiler::Lexeme lex;
+    if (argc > 2)
+      scanner.open(argv[2]);
+    else
+      scanner.open("test.pas");
+    do {
+      scanner.nextLex();
+      lex = scanner.lex();
+      std::cout << lex.row << ':'
+           << lex.column << '\t'
+           << lex.tokenName() << '\t'
+           << (unsigned long int)lex.tag << '\t'
+           << lex.name
+      << std::endl;
+    } while (lex.token != compiler::Token::END_OF_FILE && lex.token != compiler::Token::UNDEFINED);
+    return 0;
+  }
+
+  //PARSER
+  if (!std::strcmp(argv[1], "--parser") || !std::strcmp(argv[1], "-p")) {
+    compiler::Parser parser;
+    if (argc > 2)
+      parser.set(argv[2]);
+    else
+      parser.set("test.pas");
+    try {
+      parser.parse();
+      std::cout << parser.print();
+    } catch (std::exception& ex) {
+      std::cout << ex.what() << std::endl;
+    }
+    return 0;
+  }
+
+  std::cout << "unknown token: " << argv[1] << std::endl;
+  std::cout << help << std::endl;
+
+  return 0;
+}
